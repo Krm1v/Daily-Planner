5920bbd9585d2c86b90f0b05421f1989e54c7c6e
Krm1v <vladkarmalov@gmail.com>
2021-03-28 19:43:41 +0300

    28.03.2021
    - Added new launchscreen
    - Added notifications request
    - Added badges on application icon

--- Daily Planner/Daily Planner/model.swift
+++ Daily Planner/Daily Planner/model.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import UserNotifications
+import UIKit
 
 var ToDoItems: [[String: Any]] {
     set {
@@ -22,17 +24,19 @@ var ToDoItems: [[String: Any]] {
 }
 func addItem(nameItem: String, isCompleted: Bool = false) -> Void {
     ToDoItems.append(["Name": nameItem, "isCompleted": false])
-    
+    setBadge()
 }
 
 func removeItem(at index: Int) -> Void {
     ToDoItems.remove(at: index)
-    
+    setBadge()
 }
 
 func changeState(at item: Int) -> Bool {
     ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
+    setBadge()
     return ToDoItems[item]["isCompleted"] as! Bool
+    
     }
 
 func moveItem(fromIndex: Int, toIndex: Int) {
@@ -41,3 +45,22 @@ ToDoItems.remove(at: fromIndex)
 ToDoItems.insert(from, at: toIndex)
 }
 
+func requestForNotifications() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { (isEnabled, error) in
+        if isEnabled {
+            print("Accepted")
+        } else {
+            print("Denied")
+        }
+}
+}
+
+func setBadge() {
+    var totalBadgeCount = 0
+    for item in ToDoItems {
+        if (item["isCompleted"] as? Bool) == false {
+            totalBadgeCount += 1
+        }
+    }
+    UIApplication.shared.applicationIconBadgeNumber = totalBadgeCount
+}
