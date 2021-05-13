8048e31b18e3a7221ec9394c08703418bcc7620c
Krm1v <vladkarmalov@gmail.com>
2021-05-13 22:17:27 +0300

    13.05.2021
    - Changed edit and add buttons
    - Changed buttons color
    - Added new Settings button
    - Added new ViewController
    - Added new segue from TableView to ViewController
    - Grouped app files in folders
    Soon:
    - Make settings screen with notification request toggle
    - Try to adapt app to MVC design pattern
    - Some UI improvements
    - Disable opportunity to create new empty task

---
+++ Daily Planner/Daily Planner/Controllers/SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  Daily Planner
+//
+//  Created by Владислав Баранкевич on 13.05.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
--- Daily Planner/Daily Planner/Controllers/TableViewController.swift
+++ Daily Planner/Daily Planner/Controllers/TableViewController.swift
@@ -36,6 +36,17 @@ class TableViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
 
+    
+    @IBAction func settingsButton(_ sender: UIBarButtonItem) {
+        
+           
+    
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
@@ -73,6 +84,14 @@ class TableViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.imageView?.image = #imageLiteral(resourceName: "empty")
         }
+        
+        // MARK: making done todos attributed
+//        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: cell.textLabel?.text)
+//            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+//
+//        if (currentItem["isCompleted"] as? Bool) == true {
+//            cell.textLabel?.attributedText = attributeString
+//        }
 
         if tableView.isEditing {
             cell.textLabel?.alpha = 0.5
@@ -135,6 +154,8 @@ class TableViewController: UITableViewController {
         return false
     }
 }
+
+
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
